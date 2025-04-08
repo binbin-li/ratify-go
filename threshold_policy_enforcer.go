623b8a62eff17832a1a068940c3c310e8e33b875
Binbin Li <libinbin050215@gmail.com>
2025-04-08 07:07:47 +0000

    chore: chore
    Signed-off-by: Binbin Li <libinbin050215@gmail.com>

--- threshold_policy_enforcer.go
+++ threshold_policy_enforcer.go
@@ -391,6 +391,7 @@ func (e *thresholdEvaluator) createVirtualEvaluationNode(rule *ThresholdPolicyRu
 	return nodes
 }
 
+
 // Commit marks related nodes as commited.
 // In single goroutine mode, refresh commited nodes to calculate the decision.
 // In multi goroutine mode, commited node cannot be refreshed as it may need
